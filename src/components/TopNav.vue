c28071f9e342af0a3e589f910a3d381641bf4631
Tuohk <1195118385@qq.com>
2021-05-07 17:44:05 +0800

    Home頁面 CSS

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,34 @@
 <template>
   
-  <div class="topNav">
-      <div class="logo"></div>
-      <div class="menu">
-        <ul>
+  <div class="topnav">
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        
           <li>菜單1</li>
           <li>菜單2</li>
-        </ul>
-      </div>
+      
+      </ul>
     </div>
 </template>
+
+<style lang="scss" scoped>
+  .topnar{border: 1px solid red;}
+  
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style> 
