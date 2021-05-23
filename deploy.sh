13515db5aae27a207e91273405cc25666f9a3e88
Tuohk <1195118385@qq.com>
2021-05-23 23:10:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd dist
 git init
 git add .
 git commit -m "update"
-git branch -M master
+git branch -M main
 git remote add origin git@github.com:Tuohk/Yogae-ui.git
-git push -f -u origin master
+git push -f -u origin main
 cd -
